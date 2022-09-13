9e836468513608528ae6d6364f87c4a0045d11c2
josephchenhk <josephchenhk@gmail.com>
2022-09-13 17:06:38 +0800

    Add performance statistics

--- analysis.py
+++ analysis.py
@@ -19,6 +19,9 @@ import ast
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from qtrader.plugins.analysis.metrics import sharp_ratio
+from qtrader.plugins.analysis.metrics import rolling_maximum_drawdown
+
 instruments = {
     "security": {
         "Backtest": ["BTC.USD", "EOS.USD", "ETH.USD", "LTC.USD", "TRX.USD", "XRP.USD"],
@@ -34,26 +37,50 @@ instruments = {
     }
 }
 
-result_df = pd.read_csv("results/2022-09-09 12-47-25.908496/result_pairs.csv")
+result_df = pd.read_csv("results/2022-09-13 16-30-58.390740/result_pairs.csv")
 
-closes_df = pd.DataFrame(
+# # Plot normalized prices
+# closes_df = pd.DataFrame(
+#     index=result_df.datetime.apply(
+#         lambda x: datetime.strptime(
+#             ast.literal_eval(x)[0], "%Y-%m-%d %H:%M:%S")),
+#     columns=instruments["security"]["Backtest"]
+# )
+# for i, sec_code in enumerate(closes_df.columns):
+#     closes_df[sec_code] = result_df.close.apply(
+#         lambda x: ast.literal_eval(x)[0][i]
+#     ).to_list()
+#
+# range_start = datetime(2021, 7, 19)
+# range_end = datetime(2021, 8, 16)
+# closes_df = closes_df[
+#     (closes_df.index >= range_start)
+#     & (closes_df.index <= range_end)
+# ]
+# norm_closes_df = closes_df/closes_df.iloc[0]
+# norm_closes_df.plot()
+# plt.show()
+# print("-"*40)
+
+# Calculate performance
+perf_df = pd.DataFrame(
     index=result_df.datetime.apply(
         lambda x: datetime.strptime(
             ast.literal_eval(x)[0], "%Y-%m-%d %H:%M:%S")),
-    columns=instruments["security"]["Backtest"]
+    columns=["portfolio_value"]
 )
-for i, sec_code in enumerate(closes_df.columns):
-    closes_df[sec_code] = result_df.close.apply(
-        lambda x: ast.literal_eval(x)[0][i]
-    ).to_list()
-
-range_start = datetime(2021, 9, 11)
-range_end = datetime(2021, 9, 15)
-closes_df = closes_df[
-    (closes_df.index >= range_start)
-    & (closes_df.index <= range_end)
-]
-norm_closes_df = closes_df/closes_df.iloc[0]
-norm_closes_df.plot()
-plt.show()
-print()
+perf_df["portfolio_value"] = result_df.strategy_portfolio_value.apply(
+        lambda x: sum(ast.literal_eval(x))).to_list()
+perf_daily_df = perf_df.resample('D').agg({"portfolio_value": "last"})
+sr = sharp_ratio(perf_daily_df["portfolio_value"].pct_change(), 252)
+roll_mdd = rolling_maximum_drawdown(perf_daily_df['portfolio_value'])
+print(
+    "____________Performance____________\n"
+    + "Start Date: {}\n".format(perf_daily_df.index[0].strftime("%Y-%m-%d"))
+    + "End Date: {}\n".format(perf_daily_df.index[-1].strftime("%Y-%m-%d"))
+    + "Total Return: {:.2f}%\n".format(
+        (perf_daily_df["portfolio_value"].iloc[-1]
+         / perf_daily_df["portfolio_value"].iloc[0] - 1) * 100)
+    + "Sharpe Ratio: {:.2f}\n".format(sr)
+    + "Rolling Maximum Drawdown: {:.2f}%\n".format(roll_mdd.min() * 100)
+)

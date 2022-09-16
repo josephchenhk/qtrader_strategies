86a9f7583eb194935383a536ab9f214cae9ea00a
josephchenhk <josephchenhk@gmail.com>
2022-09-16 18:06:23 +0800

    Add force-reinstall to pip install

--- preparations.sh
+++ preparations.sh
@@ -4,7 +4,7 @@ conda create -n demo_strategy python=3.8 -y
 echo "Activate conda environment"
 sleep 3
 conda activate demo_strategy
-pip install --no-input git+https://github.com/josephchenhk/qtrader@master
+pip install --force-reinstall git+https://github.com/josephchenhk/qtrader@master
 pip install --no-input dill finta termcolor pyyaml func_timeout scipy statsmodels hyperopt jupyter seaborn
 echo "Python environment and packages have been installed."
 exec /bin/bash

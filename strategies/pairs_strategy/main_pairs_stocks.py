2d96beab6edec5318288ac0a49ed639dc9504ded
josephchenhk <josephchenhk@gmail.com>
2022-10-07 11:20:27 +0800

    Add stock strategy

---
+++ strategies/pairs_strategy/main_pairs_stocks.py
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+# @Time    : 9/6/2022 4:45 PM
+# @Author  : Joseph Chen
+# @Email   : josephchenhk@gmail.com
+# @FileName: main_pairs_crypto.py
+
+"""
+Copyright (C) 2020 Joseph Chen - All Rights Reserved
+You may use, distribute and modify this code under the
+terms of the JXW license, which unfortunately won't be
+written for another century.
+
+You should have received a copy of the JXW license with
+this file. If not, please write to: josephchenhk@gmail.com
+"""
+
+##########################################################################
+#                                                                        #
+#                    Pairs Trading strategy                              #
+##########################################################################
+
+"""
+{
+  "lookback_period": 2000,
+  "recalibration_lookback_ratio": 0.181,
+  "corr_init_threshold": 0.85,
+  "corr_maintain_threshold": 0.65,
+  "coint_pvalue_init_threshold": 0.01,
+  "coint_pvalue_maintain_threshold": 0.05,
+  "entry_threshold_pct": 0.75,
+  "exit_threshold_pct": 0.99,
+  "max_number_of_entry": 1,
+  "capital_per_entry": 5000000,
+  "ma_short_length": 140,
+  "ma_long_length": 200
+}
+"""
+import json
+import itertools
+from datetime import datetime
+
+import pandas as pd
+
+from qtrader.core.position import Position
+from qtrader.core.engine import Engine
+from qtrader.core.security import Stock
+from qtrader.core.event_engine import BarEventEngineRecorder, BarEventEngine
+from qtrader.core.constants import TradeMode, Exchange
+from qtrader.gateways import BacktestGateway
+from qtrader.gateways.backtest import BacktestFees
+from pairs_strategy_v2 import PairsStrategy
+
+with open("params.json", "r") as f:
+    params = json.load(f)
+
+def run_strategy(**kwargs):
+
+    trading_sessions = [
+        [datetime(1970, 1, 1, 9, 30, 0),
+         datetime(1970, 1, 1, 12, 0, 0)],
+        [datetime(1970, 1, 1, 13, 0, 0),
+         datetime(1970, 1, 1, 16, 0, 0)],
+    ]
+    gateway_name = "Backtest"
+
+    if "start" in kwargs:
+        start = kwargs["start"]
+    else:
+        start = datetime(2022, 1, 1, 0, 0, 0)
+
+    if "end" in kwargs:
+        end = kwargs["end"]
+    else:
+        end = datetime(2022, 2, 1, 0, 0, 0)
+
+    stock_list = [
+        Stock(code='HK.00175', lot_size=1000, security_name='吉利汽车'),
+        Stock(code='HK.00305', lot_size=10000, security_name='五菱汽车'),
+        Stock(code='HK.00489', lot_size=2000, security_name='东风集团股份'),
+        Stock(code='HK.00708', lot_size=500, security_name='恒大汽车'),
+        # Stock(code='HK.01114', lot_size=2000, security_name='华晨中国'),
+        Stock(code='HK.01122', lot_size=2000, security_name='庆铃汽车股份'),
+        Stock(code='HK.01211', lot_size=500, security_name='比亚迪股份'),
+        Stock(code='HK.01958', lot_size=500, security_name='北京汽车'),
+        # Stock(code='HK.02015', lot_size=100, security_name='理想汽车-W'),
+        Stock(code='HK.02238', lot_size=2000, security_name='广汽集团'),
+        Stock(code='HK.02333', lot_size=500, security_name='长城汽车'),
+        # Stock(code='HK.09863', lot_size=100, security_name='零跑汽车'),
+        # Stock(code='HK.09866', lot_size=10, security_name='蔚来-SW'),
+        # Stock(code='HK.09868', lot_size=100, security_name='小鹏汽车-W'),
+
+        # Stock(code='HK.00005', lot_size=400, security_name='汇丰控股'),
+        # Stock(code='HK.00011', lot_size=100, security_name='恒生银行'),
+        # Stock(code='HK.00023', lot_size=200, security_name='东亚银行'),
+        # Stock(code='HK.02356', lot_size=400, security_name='大新银行集团'),
+        # Stock(code='HK.02388', lot_size=500, security_name='中银香港'),
+        # Stock(code='HK.02888', lot_size=50, security_name='渣打集团')
+    ]
+
+    security_pairs = kwargs.get("security_pairs")
+    if security_pairs:
+        security_codes = []
+        [security_codes.extend(p) for p in security_pairs]
+        security_codes = list(set(security_codes))
+        stock_list = [s for s in stock_list if s.code in security_codes]
+    else:
+        security_pairs = list(
+            itertools.combinations([s.code for s in stock_list], 2))
+
+    gateway = BacktestGateway(
+        securities=stock_list,
+        start=start,
+        end=end,
+        gateway_name=gateway_name,
+        fees=BacktestFees,
+        trading_sessions={
+            security.code: trading_sessions for security in stock_list},
+    )
+
+    gateway.SHORT_INTEREST_RATE = 0.0
+    gateway.trade_mode = TradeMode.BACKTEST
+
+    # Execution engine
+    engine = Engine(gateways={gateway_name: gateway})
+
+    # Initialize strategies
+    strategy_account = "PairStrategy"
+    strategy_version = "1.0"
+    init_position = Position()
+    init_capital = params["capital_per_entry"] * len(security_pairs)
+
+    strategy = PairsStrategy(
+        securities={gateway_name: stock_list},
+        strategy_account=strategy_account,
+        strategy_version=strategy_version,
+        engine=engine,
+        strategy_trading_sessions={
+            security.code: trading_sessions for security in stock_list},
+        init_strategy_cash={gateway_name: init_capital},
+        init_strategy_position={gateway_name: init_position},
+        **kwargs
+    )
+
+    strategy.init_strategy()
+
+    # Recorder
+    recorder = BarEventEngineRecorder(datetime=[],
+                                      bar_datetime=[],
+                                      open=[],
+                                      high=[],
+                                      low=[],
+                                      close=[],
+                                      volume=[])
+
+    # Event engine
+    event_engine = BarEventEngine(
+        {"pairs": strategy},
+        {"pairs": recorder},
+        engine
+    )
+
+    # Event loop
+    event_engine.run()
+
+    # Program terminates normally
+    engine.log.info("Program shutdown normally.")
+
+    return pd.DataFrame({
+        "datetime": [datetime.strptime(d[0], "%Y-%m-%d %H:%M:%S") for d in recorder.datetime],
+        "portfolio_value": [p[0] for p in recorder.strategy_portfolio_value]}
+    )
+
+if __name__ == "__main__":
+
+    if params["load_params"] == 1:
+        # Testing period
+        start = datetime(2022, 1, 1, 0, 0, 0)
+        end = datetime(2022, 8, 1, 0, 0, 0)
+    else:
+        # Training period
+        start = datetime(2021, 4, 1, 0, 0, 0)
+        end = datetime(2022, 1, 1, 0, 0, 0)
+    df = run_strategy(
+        start=start,
+        end=end
+    )
+    print("Backtest is done.")
---
+++ strategies/pairs_strategy/optimization_pairs_stocks.py
@@ -0,0 +1,213 @@
+# -*- coding: utf-8 -*-
+# @Time    : 10/9/2022 11:36 am
+# @Author  : Joseph Chen
+# @Email   : josephchenhk@gmail.com
+# @FileName: optimization_pairs.py
+
+"""
+Copyright (C) 2020 Joseph Chen - All Rights Reserved
+You may use, distribute and modify this code under the 
+terms of the JXW license, which unfortunately won't be
+written for another century.
+
+You should have received a copy of the JXW license with
+this file. If not, please write to: josephchenhk@gmail.com
+"""
+from typing import Tuple, Dict, List
+import multiprocessing
+import pickle
+from functools import partial
+from datetime import datetime
+
+import numpy as np
+
+from hyperopt import hp
+from hyperopt import fmin
+from hyperopt import tpe
+from hyperopt import Trials
+from hyperopt import STATUS_OK
+
+from qtrader.plugins.analysis.metrics import sharpe_ratio, rolling_maximum_drawdown
+from qtrader.core.utility import timeit
+
+from main_pairs_stocks import run_strategy
+# from pandas_pairs import run_strategy
+
+
+# define an objective function
+def objective(args, **kwargs):
+    (case, recalibration_lookback_ratio, ma_short_length) = args
+    if case == 'case 1':
+        df = run_strategy(
+            security_pairs=kwargs.get("security_pairs"),
+            start=kwargs.get("start"),
+            end=kwargs.get("end"),
+            override_indicator_cfg=
+            {'params':
+                 {'recalibration_lookback_ratio': recalibration_lookback_ratio,
+                  'ma_short_length': ma_short_length,
+                 }
+             },
+        )
+        df_daily = df.set_index('datetime').resample('D').agg(
+            {"portfolio_value": "last"}).dropna()
+        sr = sharpe_ratio(
+            returns=(
+                df_daily["portfolio_value"].diff()
+                / df_daily["portfolio_value"].iloc[0]).dropna(),
+            days=256
+        )
+        tot_r = df_daily["portfolio_value"].iloc[-1] / df["portfolio_value"].iloc[0] - 1.0
+        mdd = rolling_maximum_drawdown(
+            portfolio_value=df_daily["portfolio_value"].to_numpy(),
+            window=365
+        ).iloc[-1]
+        if mdd == 0:
+            RoMaD = -np.Inf
+        else:
+            RoMaD = -tot_r / mdd
+        return {
+            'loss': -sr,
+            'status': STATUS_OK,
+            'sharpe_ratio': sr,
+            'total_return': tot_r,
+            'maximum_drawdown': mdd,
+            'return_over_maximum_drawdown': RoMaD
+        }
+
+
+def worker(
+        security_pairs: List[Tuple[str]],
+        start: datetime,
+        end: datetime,
+) -> Dict[str, float]:
+    """Process that run the optimization for a pair"""
+    trials = Trials()
+    best = timeit(fmin)(
+        partial(objective,
+                security_pairs=security_pairs,
+                start=start,
+                end=end),
+        space,
+        algo=tpe.suggest,
+        max_evals=30,
+        trials=trials
+    )
+    opt_params = {}
+    for security_pair in security_pairs:
+        opt_params[security_pair] = {
+            'recalibration_lookback_ratio': best['recalibration_lookback_ratio'],
+            'ma_short_length': ma_short_length_choice[best['ma_short_length']],
+            'best_loss': trials.best_trial['result']['loss'],
+            'sharpe_ratio': trials.best_trial['result']['sharpe_ratio'],
+            'total_return': trials.best_trial['result']['total_return'],
+            'maximum_drawdown': trials.best_trial['result']['maximum_drawdown'],
+            'return_over_maximum_drawdown': trials.best_trial['result'][
+                'return_over_maximum_drawdown'],
+        }
+    print(opt_params)
+    # Save to pkl file
+    start_str = start.strftime("%Y%m%d")
+    end_str = end.strftime("%Y%m%d")
+    with open(
+            f"opt_params/opt_params_{start_str}_{end_str}.pkl",
+            "wb") as f:
+        pickle.dump(opt_params, f)
+    print(f"Optimization for {start_str}-{end_str} is done.")
+    return opt_params
+
+# define a search space
+ma_short_length_choice = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140]
+space = hp.choice('a', [
+      ('case 1',
+       hp.uniform('recalibration_lookback_ratio', 0.01, 0.30),
+       hp.choice('ma_short_length', ma_short_length_choice),
+       )]
+)
+
+# minimize the objective over the space
+security_pairs_lst = [
+    ('HK.00175', 'HK.00305'),
+    ('HK.00175', 'HK.00489'),
+    ('HK.00175', 'HK.00708'),
+    ('HK.00175', 'HK.01122'),
+    ('HK.00175', 'HK.01211'),
+    ('HK.00175', 'HK.01958'),
+    ('HK.00175', 'HK.02238'),
+    ('HK.00175', 'HK.02333'),
+    ('HK.00305', 'HK.00489'),
+    ('HK.00305', 'HK.00708'),
+    ('HK.00305', 'HK.01122'),
+    ('HK.00305', 'HK.01211'),
+    ('HK.00305', 'HK.01958'),
+    ('HK.00305', 'HK.02238'),
+    ('HK.00305', 'HK.02333'),
+    ('HK.00489', 'HK.00708'),
+    ('HK.00489', 'HK.01122'),
+    ('HK.00489', 'HK.01211'),
+    ('HK.00489', 'HK.01958'),
+    ('HK.00489', 'HK.02238'),
+    ('HK.00489', 'HK.02333'),
+    ('HK.00708', 'HK.01122'),
+    ('HK.00708', 'HK.01211'),
+    ('HK.00708', 'HK.01958'),
+    ('HK.00708', 'HK.02238'),
+    ('HK.00708', 'HK.02333'),
+    ('HK.01122', 'HK.01211'),
+    ('HK.01122', 'HK.01958'),
+    ('HK.01122', 'HK.02238'),
+    ('HK.01122', 'HK.02333'),
+    ('HK.01211', 'HK.01958'),
+    ('HK.01211', 'HK.02238'),
+    ('HK.01211', 'HK.02333'),
+    ('HK.01958', 'HK.02238'),
+    ('HK.01958', 'HK.02333'),
+    ('HK.02238', 'HK.02333')
+]
+
+if __name__ == "__main__":
+
+    dates = [
+        # (datetime(2020, 7, 1), datetime(2021, 1, 1)),
+        # (datetime(2020, 8, 1), datetime(2021, 2, 1)),
+        # (datetime(2020, 9, 1), datetime(2021, 3, 1)),
+        # (datetime(2020, 10, 1), datetime(2021, 4, 1)),
+        # (datetime(2020, 11, 1), datetime(2021, 5, 1)),
+        # (datetime(2020, 12, 1), datetime(2021, 6, 1)),
+        # (datetime(2021, 1, 1), datetime(2021, 7, 1)),
+        # (datetime(2021, 2, 1), datetime(2021, 8, 1)),
+        # (datetime(2021, 3, 1), datetime(2021, 9, 1)),
+        # (datetime(2021, 4, 1), datetime(2021, 10, 1)),
+        # (datetime(2021, 5, 1), datetime(2021, 11, 1)),
+        # (datetime(2021, 6, 1), datetime(2021, 12, 1)),
+        # (datetime(2021, 7, 1), datetime(2022, 1, 1)),
+        # (datetime(2021, 8, 1), datetime(2022, 2, 1)),
+        # (datetime(2021, 9, 1), datetime(2022, 3, 1)),
+        # (datetime(2021, 10, 1), datetime(2022, 4, 1)),
+        # (datetime(2021, 11, 1), datetime(2022, 5, 1)),
+        # (datetime(2021, 12, 1), datetime(2022, 6, 1)),
+        # (datetime(2022, 1, 1), datetime(2022, 7, 1)),
+        # (datetime(2022, 2, 1), datetime(2022, 8, 1)),
+
+        (datetime(2021, 4, 1), datetime(2022, 1, 1)),
+        (datetime(2021, 6, 1), datetime(2022, 3, 1)),
+        (datetime(2021, 8, 1), datetime(2022, 5, 1)),
+        (datetime(2021, 10, 1), datetime(2022, 7, 1)),
+    ]
+
+    manager = multiprocessing.Manager()
+    jobs = []
+    for start, end in dates:
+        start_str = start.strftime("%Y%m%d")
+        end_str = end.strftime("%Y%m%d")
+
+        # Parallel
+        p = multiprocessing.Process(
+            target=worker,
+            args=(security_pairs_lst, start, end))
+        jobs.append(p)
+        p.start()
+
+    for proc in jobs:
+        proc.join()
+    print(f"Optimization is all done.")
